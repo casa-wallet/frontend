511c21a0db03a9c67f47b1450e0d6901a6ca26aa
hospodin <35768075+hospodin@users.noreply.github.com>
2024-07-13 20:25:16 +0300

    added all chains

--- src/consts/smartAccounts.ts
+++ src/consts/smartAccounts.ts
@@ -1,6 +1,66 @@
 import { KernelSmartAccountLib } from '@/lib/smart-accounts/KernelSmartAccountLib'
 import { SafeSmartAccountLib } from '@/lib/smart-accounts/SafeSmartAccountLib'
-import { goerli, polygonMumbai, sepolia, arbitrumSepolia, baseSepolia, celoAlfajores, scrollSepolia, lineaSepolia, neonDevnet, rootstockTestnet, zircuitTestnet } from 'viem/chains'
+import {
+  goerli,
+  polygonMumbai,
+  sepolia,
+  // CASA
+  arbitrumSepolia,
+  baseSepolia,
+  celoAlfajores,
+  scrollSepolia,
+  neonDevnet,
+  rootstockTestnet,
+  zircuitTestnet,
+} from 'viem/chains'
+
+import { defineChain } from 'viem'
+
+
+const apechainJenkins = defineChain({
+  id: 1798,
+  name: "ApeChain Jenkins Testnet",
+  nativeCurrency: { name: 'ApeCoin', symbol: 'APE', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://jenkins.rpc.caldera.xyz/http'],
+    },
+  },
+  testnet: true
+})
+
+const morphHolesky = defineChain({
+  id: 2810,
+  name: 'Morph Holesky',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-holesky.morphl2.io'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Morph Testnet Explorer',
+      url: 'https://explorer-holesky.morphl2.io',
+      apiUrl: 'https://explorer-api-holesky.morphl2.io/api',
+    },
+  },
+  testnet: true,
+})
+
+const zeroSepolia = defineChain({
+  id: 4457845,
+  name: 'ZERϴ Testnet Sepolia',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.zerion.io/v1/zero-sepolia'],
+    },
+  },
+  testnet: true,
+})
+
+
 
 // Types
 export const allowedChains = [
@@ -11,7 +71,9 @@ export const allowedChains = [
   baseSepolia,
   celoAlfajores,
   scrollSepolia,
-  lineaSepolia,
+  apechainJenkins,
+  morphHolesky,
+  zeroSepolia,
   neonDevnet,
   rootstockTestnet,
   zircuitTestnet
